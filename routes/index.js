240c0be9b37987b32289e44c3b4d3b564d61a386
Laurent Bouhnik <misterbl@hotmail.com>
2017-01-22 13:25:44 +0000

    more tests and stylesheet

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,21 @@
 var express = require('express');
 var router = express.Router();
 var Amount = require('../public/javascripts/amount.js');
-var session = require('express-session');
-var sess;
+
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    console.log("hello");
   res.render('index');
 });
 
 router.post('/', function (req, res) {
   var amount = new Amount(req.body.amount);
   if (amount.checkAmount() != false){
-  res.render('result', {amount: amount, input: req.body.amount});
-}
-else if (amount.checkAmount() == false) {
-  console.log("hello2");
-res.render('result', {amount: null});
-}
+    res.render('result', {amount: amount, input: req.body.amount});
+  }
+  else if (amount.checkAmount() == false) {
+    res.render('result', {amount: null});
+  }
 });
 
 
